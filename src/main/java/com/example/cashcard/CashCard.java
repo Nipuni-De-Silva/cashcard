1e5d6ca4db47989831da6022145136336489c7ea
Nipuni De Silva <nipunisha274@gmail.com>
2023-12-27 23:55:27 +0530

    CashCardRepository file

--- src/main/java/com/example/cashcard/CashCard.java
+++ src/main/java/com/example/cashcard/CashCard.java
@@ -1,4 +1,13 @@
 package com.example.cashcard;
 
-record CashCard(Long id, Double amount) {
+import org.springframework.data.annotation.Id;
+
+record CashCard(@Id Long id, Double amount) {
 }
+
+// reason for above errors?
+// 1. CashCard.java is not a valid java file. It is a record. It is a new feature in java 14.
+// 2. CashCardApplicationTests.java is not a valid java file. It is a test file. It is a new feature in java 14.
+// 3. CashcardApplication.java is not a valid java file. It is a java file. It is a new feature in java 14.
+// 4. CashCardController.java is not a valid java file. It is a java file. It is a new feature in java 14.
+
